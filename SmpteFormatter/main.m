0c70204dbb36109bd3dce7e049f87cd8c6508297
Christian Floisand <christian@christianfloisand.com>
2013-05-22 09:49:57 -0400

    Minor edits.

--- SmpteFormatter/main.m
+++ SmpteFormatter/main.m
@@ -1,12 +1,31 @@
-//
-//  main.m
-//  SmpteFormatter
-//
-//  Created by Christian on 2013-05-22.
-//  Copyright (c) 2013 Rainland Audio. All rights reserved.
-//
+/*!
+ 
+ \file      main.m
+ 
+ \author    Christian Floisand
+ \version   1.0
+ \date      Created: 2013/05/22
+            Updated: 2013/05/22
+ \copyright Copyright (C) 2013  Christian Floisand
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
 
 #import <Cocoa/Cocoa.h>
+
 
 int main(int argc, char *argv[])
 {
